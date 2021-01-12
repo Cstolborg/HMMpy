eb4df8bbb7d0429bed0f5229c01a61087b8ea8c6
Cstolborg <christianstolborg@gmail.com>
2021-01-12 13:22:13 +0100

    Added dof_estimator to get degrees of freedom in t-dist.

--- thmm.py
+++ thmm.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy import stats
+from scipy.special import digamma
 import matplotlib.pyplot as plt
 
 from utils import simulate_2state_gaussian
-from Main import BaseHiddenMarkov
+from base_hmm import BaseHiddenMarkov
+
+import scipy.optimize as opt
 
 """ TODO
 
@@ -55,25 +58,19 @@ class GaussianHMM(BaseHiddenMarkov):
         probs = np.exp(log_probs)
         return probs, log_probs
 
-    def _m_step(self, X, u, f):
+    def _m_step(self, X, u, f, iterations):
         ''' Given u and f do an m-step.
           Update degrees of freedom iteratively.
          Updates the model parameters delta, Transition matrix and state dependent distributions.
          '''
-        # Empty list to store u_it values
-
-        def object_fun(X):
-            return X
-
-        opt.root(object_fun, )
-
+        X = np.array(X)
         T = len(X)
+
+        # Empty list to store u_it values
         self.u_it = np.zeros((T, self.n_states))  # Init N X M matrix
 
-        X = np.array(X)
         # Instantiating the u_it
         self.u_it[0, :] = (np.square(self.std) * (self.dof + 1) / (np.square(self.std) * self.dof + np.square((X[0] - self.mu))))
-        print('First u_it: ', self.u_it)
         # df to be updated to formula on page 34 in Nystrup Master Thesis
 
         # Update transition matrix and initial probs
@@ -82,14 +79,24 @@ class GaussianHMM(BaseHiddenMarkov):
 
 
         # Update state-dependent distributions
-        for j in range(self.n_states):
-            self.mu[j] = np.sum(u[:, j] * X * self.u_it[:, j]) / (np.sum(u[:, j]*self.u_it[:,j]))
-            self.std[j] = np.sqrt(np.sum(u[:, j] * np.square(X - self.mu[j])) / np.sum(u[:, j]))
-            for t in range(1, T):
-                self.u_it[t, j] = np.square(self.std[j]) * (self.dof + 1) / ((np.square(self.std[j]) * self.dof) + np.square(X[t] - self.mu[j]))
+        for iteration in range(iterations):
+            for j in range(self.n_states):
+                if j == 0:
+                    self.mu[j] = np.sum(u[:, j] * X * self.u_it[:, j]) / (np.sum(u[:, j]*self.u_it[:, j]))
+                    self.std[j] = np.sqrt(np.sum(u[:, j] * np.square(X - self.mu[j])) / np.sum(u[:, j]))
+                    self.u_it[:, j] = np.square(self.std[j]) * (self.dof + 1) / ((np.square(self.std[j]) * self.dof) + np.square(X - self.mu[j]))
 
-            self.dof[j] = 1-stats.special.digamma(0.5*self.dof[j])+log(0.5*self.dof[j])+stats.special.digamma((self.dof[j]+1)/2)-log((self.dof[j]+1)/2)+1/np.sum(u[:,j])*(np.sum(u[:,j]*(log(self.u_it[t,j]-self.u_it[t,j])))
-            opt.root(f, ...)
+                    # Find root of some estimator function based on digammas and dof
+                    def dof_estimator(dof):  # TODO research why Nystrup master p. 94 use log(dof) rather than dof
+                        term1 = 1-digamma(0.5*dof)
+                        term2 = np.log(0.5*dof)
+                        term3 = digamma((dof+1)/2)
+                        term4 = -np.log((dof+1)/2)
+                        term5 = 1 / np.sum(u[:, j]) * np.sum(u[:, j] * (np.log(self.u_it[:, j])-self.u_it[:, j]))
+                        return term1 + term2 + term3 + term4 + term5
+
+                    self.dof = opt.root(fun=dof_estimator, x0=self.dof).x
+
 
     def fit(self, X, verbose=0):
         """Iterates through the e-step and the m-step"""
@@ -135,9 +142,12 @@ if __name__ == '__main__':
 
     #model.fit(returns, verbose=0)
     u, f, llk = model._e_step(returns)
-    model._m_step(returns, u, f)
-    print("u_it = ", model.u_it)
-    print("mu =", model.mu)
-    print("std = ",model.std)
+    model._m_step(returns, u, f, iterations=20)
+
+    #print("u_it = ", model.u_it)
+    #print("mu =", model.mu)
+    #print("std = ",model.std)
+
+
     #states, posteriors = model.predict(returns)
 
