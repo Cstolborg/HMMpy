560d355111afbb7bdfb2b8503fbde12f4308123c
Mathiasrjoergensen <mathiasrjoergensen@gmail.com>
2021-01-15 19:21:07 +0100

    Right local mean

--- models/jump_hmm.py
+++ models/jump_hmm.py
@@ -30,14 +30,27 @@ class JumpHMM(BaseEstimator):
         Y[1:, 1] = np.abs(np.diff(X))
         Y[:-1, 2] = np.abs(np.diff(X))
 
+        x_1 = np.convolve(X, np.ones(N) / 3, mode='valid')
+
+        def running_mean(X, N):
+            cumsum = np.cumsum(np.insert(X, 0, 0))
+            return (cumsum[3:] - cumsum[:-3]) / float(3) ## Window lenth = the numbers represented here!! (This is equivalent to the right local mean)
+
+
         # Centered moments
         #start_idx, stop_idx = int(window_len/2), -int(window_len/2)
-        #Y[start_idx:stop_idx, 3] =
+        #Y[start_idx:stop_idx, 3]
         #vec = np.cumsum(np.insert(X, 0, 0))
         #print((vec[start_idx:] - vec[:-stop_idx]) / (window_len / 2 + 2) )
         #Y[(start_idx+1):(stop_idx-1), 3] = (vec[start_idx:] - vec[:-stop_idx]) / (window_len / 2 + 2)
 
         print(Y)
+        print("Left absolute change = ",Y[1:, 1])
+        print('Right absolute change =', Y[:-1, 2])
+        print("x_1 =", x_1)
+        print("cumsum = ", running_mean(X,N))
+        print("N =", N)
+
 
     def loss(self, X, theta):
         return np.sqrt(np.sum(np.square(X - theta)))
@@ -47,5 +60,4 @@ if __name__ == '__main__':
     #returns, true_regimes = simulate_2state_gaussian(plotting=False)  # Simulate some data from two normal distributions
     returns = np.arange(1,10,1)
     returns = returns ** 2
-
     model.construct_features(returns, window_len= 2)
