7683a840c28545d55de14e6aa5fbf895f8e6d368
Mathiasrjoergensen <mathiasrjoergensen@gmail.com>
2021-01-16 16:04:45 +0100

    Right local mean + right std

--- models/jump_hmm.py
+++ models/jump_hmm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import ndarray
+import pandas as pd
 from scipy import stats
 from sklearn.base import BaseEstimator
 import matplotlib.pyplot as plt
@@ -23,8 +24,9 @@ class JumpHMM(BaseEstimator):
     def construct_features(self, X: ndarray, window_len: int):
         N = len(X)
         Y = np.zeros(shape=(len(X), 9))
-
+        df = pd.DataFrame(Y, columns=list('123456789'))
         Y[:, 0] = X  # Observations
+
 
         # Absolute changes
         Y[1:, 1] = np.abs(np.diff(X))
@@ -32,9 +34,17 @@ class JumpHMM(BaseEstimator):
 
         x_1 = np.convolve(X, np.ones(N) / 3, mode='valid')
 
-        def running_mean(X, N):
-            cumsum = np.cumsum(np.insert(X, 0, 0))
-            return (cumsum[3:] - cumsum[:-3]) / float(3) ## Window lenth = the numbers represented here!! (This is equivalent to the right local mean)
+        def right_mean(X, N):
+            R_m = np.cumsum(np.insert(X, 0, window_len)) # Cumulative sum for a given window
+            return (R_m[window_len:] - R_m[:-window_len]) / float(window_len)
+
+        def right_std(X, N):
+            #R_s = np.cumsum((np.insert(X, 0, window_len)))
+            #Sum_obs = R_s[window_len:]-R_s[:-window_len] #Sum of X observations
+            #Mean_obs = right_mean(X,N)
+            std_roll = df
+            moving_std = std_roll.rolling(window_len).std(ddof=0) #note standard is 1 (sample)
+            return moving_std
 
 
         # Centered moments
@@ -43,13 +53,16 @@ class JumpHMM(BaseEstimator):
         #vec = np.cumsum(np.insert(X, 0, 0))
         #print((vec[start_idx:] - vec[:-stop_idx]) / (window_len / 2 + 2) )
         #Y[(start_idx+1):(stop_idx-1), 3] = (vec[start_idx:] - vec[:-stop_idx]) / (window_len / 2 + 2)
-
         print(Y)
         print("Left absolute change = ",Y[1:, 1])
         print('Right absolute change =', Y[:-1, 2])
-        print("x_1 =", x_1)
-        print("cumsum = ", running_mean(X,N))
-        print("N =", N)
+        print("Right mean = ", right_mean(X,N))
+        print("Right std = ", right_std(X,N))
+
+
+
+
+
 
 
     def loss(self, X, theta):
@@ -60,4 +73,4 @@ if __name__ == '__main__':
     #returns, true_regimes = simulate_2state_gaussian(plotting=False)  # Simulate some data from two normal distributions
     returns = np.arange(1,10,1)
     returns = returns ** 2
-    model.construct_features(returns, window_len= 2)
+    model.construct_features(returns, window_len= 3)
