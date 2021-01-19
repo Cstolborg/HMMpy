200c7dc0b5f4f97212e9b9cbfafc46213892d23b
Cstolborg <christianstolborg@gmail.com>
2021-01-19 10:57:52 +0100

    Added init_params

--- models/jump_hmm.py
+++ models/jump_hmm.py
@@ -10,9 +10,9 @@ from utils.simulate_returns import simulate_2state_gaussian
 
 class JumpHMM(BaseEstimator):
 
-    def __init__(self, n_states: int = 2, max_iter: int = 100, tol: int = 1e-4,
+    def __init__(self, n_states_: int = 2, max_iter: int = 100, tol: int = 1e-4,
                  epochs: int = 1, random_state: int = 42):
-        self.n_states = n_states
+        self.n_states_ = n_states_
         self.random_state = random_state
         self.max_iter = max_iter  # Max iterations to fit model
         self.epochs = epochs  # Set number of random inits used in model fitting
@@ -23,7 +23,7 @@ class JumpHMM(BaseEstimator):
 
     def construct_features(self, X: ndarray, window_len: int, num_features = 9):
         N = len(X)
-        Y = np.zeros(shape=(len(X), num_features))
+        #Y = np.zeros(shape=(len(X), num_features))
         df = pd.DataFrame(X)
 
 
@@ -38,22 +38,36 @@ class JumpHMM(BaseEstimator):
         df['Central local mean'] = (look_ahead + look_back) / (2 * window_len)
         df['Centered local std'] = df[0].rolling(window_len * 2).std(ddof=0).shift(-window_len)  # Rolls from 0 and 2x length iteratively, then shifts back 1x window length
 
-
         # Absolute changes
         #Y[1:, 1] = np.abs(np.diff(X))
         #Y[:-1, 2] = np.abs(np.diff(X))
+        Z = df.dropna().to_numpy()
+        self.n_features_ = Z.shape[1]
 
-        print(df)
+        return Z
+
+    def _init_params(self, Z):
+        # State sequence
+        state_index = np.arange(start=0, stop=self.n_states_, step=1, dtype=int)  # Array of possible states
+        state_seq = np.random.choice(a=state_index, size=len(Z))  # Sample sequence uniformly
+        self.state_seq_ = state_seq
+
+        # Theta
+        self.theta_ = np.zeros(shape=(self.n_features_, self.n_states_))  # Init as empty matrix
 
 
-
+    def _fit_theta(self, Z):
+        pass
 
     def loss(self, X, theta):
         return np.sqrt(np.sum(np.square(X - theta)))
 
 if __name__ == '__main__':
-    model = JumpHMM(n_states=2)
-    #returns, true_regimes = simulate_2state_gaussian(plotting=False)  # Simulate some data from two normal distributions
-    returns = np.arange(1,10,1)
-    returns = returns ** 2
-    model.construct_features(returns, window_len= 3)
+    model = JumpHMM(n_states_=2)
+    returns, true_regimes = simulate_2state_gaussian(plotting=False)  # Simulate some data from two normal distributions
+    #returns = np.arange(1,10,1)
+    #returns = returns ** 2
+
+    Z = model.construct_features(returns, window_len= 3)
+
+    model._init_params(Z)
