2c5da76dd4a13ebab779b8c908c75562d53d44bd
mathiasrjoergensen <Mathiasrjoergensen@gmail.com>
2021-02-04 18:40:12 +0100

    Finance update

--- finance/dynamic_portfolio_optimization.py
+++ finance/dynamic_portfolio_optimization.py
@@ -16,12 +16,10 @@ state_sequence = [0,0,1,0,1,0,1,1,1,1]
 Weight_asset = np.zeros(shape=(len(state_sequence), 8))  # Init as empty matrix
 
 for i in range(len(state_sequence)):
-    if state_sequence == 0:
-        Weight_asset = ([0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125])
-    elif state_sequence == 1:
-        Weight_asset = ([0, 0.30, 0, 0.25, 0, 0.15, 0, 0.3])
-
-
+    if state_sequence[i] == 0:
+        Weight_asset[i] = ([0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125])
+    elif state_sequence[i] == 1:
+        Weight_asset[i] = ([0, 0.30, 0, 0.25, 0, 0.15, 0, 0.3])
 print(Weight_asset)
 
 
